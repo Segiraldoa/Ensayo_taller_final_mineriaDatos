f5ac4283618024990c34013b1a5c2a3d8002e75f
Segiraldoa <93294390+Segiraldoa@users.noreply.github.com>
2025-02-11 02:50:30 -0500

    Update heartdisease.py

--- heartdisease.py
+++ heartdisease.py
@@ -393,7 +393,6 @@ if st.sidebar.checkbox("Utilizar redes Neuronales"):
             encoded_df = pd.DataFrame(encoded_array, columns=encoder.get_feature_names_out())            
             # Concatenar las variables numéricas con las categóricas codificadas
             final_data = pd.concat([new_data_numerical, encoded_df], axis=1)  
-            st.write(type(final_data))
             prediction=np.argmax(model2.predict(final_data))
             if prediction==1:
                 st.write("Predicción del modelo:","Cath", prediction)
@@ -470,8 +469,8 @@ if st.sidebar.checkbox("Utilizar redes Neuronales"):
                 encoded_df = pd.DataFrame(encoded_array, columns=encoder.get_feature_names_out())            
                 # Concatenar las variables numéricas con las categóricas codificadas
                 final_data = pd.concat([new_data_numerical, encoded_df], axis=1)  
-                st.write(type(final_data))
                 prediction=np.argmax(model2.predict(final_data))
+                st.write(prediction)
                 if prediction==1:
                     st.write("Predicción del modelo:","Cath", prediction)
                 else:
